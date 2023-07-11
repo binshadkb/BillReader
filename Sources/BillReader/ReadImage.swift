6232de8ccb38d55c423f78deb21cd9240e29cfc6
Binshad <kbbinshad@gmail.com>
2023-07-11 13:17:29 +0100

    fixed string order from image

--- Sources/BillReader/ReadImage.swift
+++ Sources/BillReader/ReadImage.swift
@@ -25,9 +25,23 @@ struct ReadImage {
             guard let observations = request.results as? [VNRecognizedTextObservation] else {
                 return
             }
+            
+            let sorted = observations.sorted { (d1, d2) in
+
+                let xmin1 = floor(d1.boundingBox.origin.x * image.size.width)
+                let xmin2 = floor(d2.boundingBox.origin.x * image.size.width)
+                let ymin1 = floor((1 - d1.boundingBox.origin.y - d1.boundingBox.size.height) * image.size.height)
+                let ymin2 = floor((1 - d2.boundingBox.origin.y - d2.boundingBox.size.height) * image.size.height)
+                
+                if (ymin1 - 10)...(ymin1 + 10) ~= ymin2 {
+                    return xmin1 < xmin2
+                }
+
+                return ymin1 < ymin2
+            }
 
             
-            let recognizedStrings = observations.compactMap { observation in
+            let recognizedStrings = sorted.compactMap { observation in
                 return observation.topCandidates(1).first?.string
             }
             
